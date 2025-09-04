a8ece7ab1c30b3b8531593b51692c204a8d5aa67
James Lai <deviousy@gmail.com>
2025-09-05 01:11:22 +0800

    flex width layout added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,18 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
-};
-
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>Dashboard{children}</div>
+  return <div className="h-screen flex">
+    {/* LEFT */}
+    <div className='w-[14%] md:w[8%] lg:w-[16%] xl:w-[14%] bg-red-200'>l</div>
+    {/* RIGHT */}
+    <div className='w-[86%] md:[92%] lg:w-[84%] xl:w-[86%] bg-blue-200'>r</div>
+  </div>
 }
