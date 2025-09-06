881ffe231432667aea1ff4ee30c9c34796b24f46
James Lai <deviousy@gmail.com>
2025-09-06 13:46:10 +0800

    parent, teacher page ready

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -22,7 +22,7 @@ export default function DashboardLayout({
         <Menu/>
       </div>  
       {/* RIGHT */}
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#cbcdd2] overflow-scroll '>
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#cbcdd2] overflow-scroll flex flex-col'>
         <Navbar/>
         {children}      
       </div>
