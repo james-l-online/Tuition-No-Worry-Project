c1372a9ceb115b7e9e78da6d3d11d95908efd3f5
James Lai <deviousy@gmail.com>
2025-09-06 13:37:51 +0800

    student page ready

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,24 @@
+"use client"
+import Announcement from "@/components/Announcement"
+import EventCalendar from "@/components/EventCalendar"
+import BigCalendar from "@/components/BigCalendar"
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+    <div className="w-full xl:w-2/3">
+    <div className="h-full bg-white p-4 rounded-md">
+      <h1 className="text-xl font-semibold">Schedule (4a)</h1>
+      <BigCalendar />
+      </div>
+    </div>
+      {/* RIGHT */}
+    <div className="w-full xl:w-1/3 flex flex-col gap-8">
+      <EventCalendar />
+      <Announcement />
+    </div>
+  </div> 
   )
 }
 
