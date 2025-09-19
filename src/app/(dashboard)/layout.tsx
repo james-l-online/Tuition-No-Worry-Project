64d0d8498af8c0ce694952b0a158fabcf7b726e3
James Lai <deviousy@gmail.com>
2025-09-19 14:57:41 +0800

    refactor: update component imports and enhance layout styling for consistency; fix component names

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
-import Menu from "@/components/Menu"; 
-import Image from "next/image";
+import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function DashboardLayout({
@@ -11,20 +11,20 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4'>
-        <Link 
-          href="/" 
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32}/>
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">Tuition No Worry</span>
         </Link>
-        <Menu/>
-      </div>  
+        <Menu />
+      </div>
       {/* RIGHT */}
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#cbcdd2] overflow-scroll flex flex-col'>
-        <Navbar/>
-        {children}      
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
       </div>
     </div>
   );
