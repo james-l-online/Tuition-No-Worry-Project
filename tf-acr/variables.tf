f8feb1236f79e64f822ff53a05d235bbf99671a5
james-l-online <226829545+james-l-online@users.noreply.github.com>
2025-10-05 16:09:38 +0800

    Define variables for ACR and resource group
    Added variables for resource group, ACR name, and location.

--- tf-acr/variables.tf
+++ tf-acr/variables.tf
@@ -1,3 +1,29 @@
+variable "resource_group_name" {
+  type    = string
+  default = "tnw-rg"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "tnwregistry"
+}
+
+variable "location" {
+  type    = string
+  default = "eastasia"
+}
+
+variable "storage_account_name" {
+  description = "Optional storage account name for backend lookups"
+  type        = string
+  default     = ""
+}
+
+variable "storage_account_rg" {
+  description = "Resource group for external storage account used by backends"
+  type        = string
+  default     = ""
+}
 variable "storage_account_name" {
   description = "Optional: name of the storage account used for Terraform backends (provisioned in tf-aks-storage)"
   type        = string
