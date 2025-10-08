3b648c04b59bf8be91a19481dc5c33c3a4ce54cd
James Lai <deviousy@gmail.com>
2025-10-08 19:54:33 +0800

    chore(tf): remove duplicate variables in tf-acr/variables.tf for demo

--- tf-acr/variables.tf
+++ tf-acr/variables.tf
@@ -13,17 +13,6 @@ variable "location" {
   default = "eastasia"
 }
 
-variable "storage_account_name" {
-  description = "Optional storage account name for backend lookups"
-  type        = string
-  default     = ""
-}
-
-variable "storage_account_rg" {
-  description = "Resource group for external storage account used by backends"
-  type        = string
-  default     = ""
-}
 // Optional storage account lookup used when backends are provisioned in a separate module
 variable "storage_account_name" {
   description = "Optional: name of the storage account used for Terraform backends (provisioned in tf-aks-storage)"
